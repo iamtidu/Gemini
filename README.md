904577b8a97ef5572c51ef14f15e0754d3dfb6a3
ANANYA BHARATI <bitturajsingh160@gmail.com>
2025-04-17 11:22:55 +0530

    Project done

---
+++ README.md
@@ -0,0 +1,8 @@
+# Gemini + Pixels 
+
+In this project I use Reactjs(Vite) Gimini and Pixels 
+
+## Outputs
+
+![imag 1](screenshots/home.png)
+![imag 1](screenshots/output.png)
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import './App.css'
+import React from "react";
+import SearchComponent from "./SearchComponent";
+
+const App = () => {
+  return (
+    <div className="App">
+      <SearchComponent />
+    </div>
+  );
+};
+
+export default App;
